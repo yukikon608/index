e7a60546d48a6d9ae7c9c909970b16f6610d7e8c
yukikon608 <77250377+yukikon608@users.noreply.github.com>
2021-01-12 10:38:36 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function () {
+    $(".btn").on("click", function () {
+        $(this).closest("div").css("display", "none");
+        id = $(this).attr("href");
+        $(id).addClass("fit").show("slow");
+    });
+});
